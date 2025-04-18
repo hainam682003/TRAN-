64502089ce76015685f2f970b941fd729bc03f1f
hainam682003 <hocon06082003@gmail.com>
2025-04-19 00:10:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
     introTitle: 'bạn à !',
     introDesc: `bạn mai nhớ đón tôi đi học nha bn 🤣🤣`,
-    btnIntro: 'hihi',
-    title: 'mai bạn khao tôi trà sữa là ăn sáng nữa nha bn 😝😝 😙',
+    btnIntro: 'hihihi💗',
+    title: 'mai bạn khao tôi trà sữa và ăn sáng nữa nha bn 😝😝 😙',
     desc: ' bạn lên đồng ý đi chứ ko có đc đâu ',
     btnYes: 'Okiii bn luôn 💕',
     btnNo: 'Không, đc đâu bn à :3',
